1644543e581c3fbd1f3686ed600cc5c673e5c6ee
kmatthes <katja.matthes@initos.com>
2014-04-16 13:53:42 +0200

    add license

---
+++ README.md
@@ -0,0 +1,9 @@
+LinkColletion plugin for MantisBT
+=================================
+
+Collect links from bugnotes and show them in bug view. A view about all links of a project is also available.
+
+License
+-------
+
+GPL3 or later
--- pages/links_view_inc.php
+++ pages/links_view_inc.php
@@ -1,5 +1,22 @@
 <?php
-	#	TODO sichtbare Links anhand der Zugriffsrechte des aktuellen Nutzers filtern
+/*
+ This file is part of LinkCollection.
+
+BoxesAsTabs is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BoxesAsTabs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BoxesAsTabs.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#	TODO sichtbare Links anhand der Zugriffsrechte des aktuellen Nutzers filtern
 
 # get the LinkCollection data of the current bug
 $t_collected_links = linkcollection_get_collection_bug( $p_bug_id, TRUE); #TODO sichtbare Links filtern
